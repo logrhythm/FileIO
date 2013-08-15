3478b28b4d8928a11674d9a31e65710b593e150b
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2013-08-15 16:55:29 -0600

    Added unit test for new helper files FileIO (read and write files) Updated ConfNtp to reflect the changes

---
+++ FileIO.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   FileIO.h
+ * Author: kjell
+ *
+ * Created on August 15, 2013, 2:28 PM
+ */
+
+#pragma once
+#include <string>
+
+namespace FileIO {   
+   template<typename T> struct Result {
+      const T result;
+      const std::string error;
+      /**
+       * Result of a FileIO operation. 
+       * @param output whatever the expected output would be
+       * @param err error message to the client, default is empty which means successful operation
+       */
+      Result(T output, const std::string& err = {""}) 
+       : result(output), error(err){}      
+   };
+   
+   Result<std::string> ReadAsciiFileContent(const std::string& pathToFile); 
+   Result<bool> WriteAsciiFileContent(const std::string& pathToFile, const std::string& content);
+}
+
