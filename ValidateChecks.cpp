d5b2afcfd9d36507e7abbd2a53dd25cd323b7ce1
Ryan <ryan.kophs@logrhythm.com>
2013-11-15 12:47:49 -0700

    includes again

--- ValidateChecks.cpp
+++ ValidateChecks.cpp
@@ -1,22 +1,24 @@
 #include "ValidateChecks.h"
+#include <sstream>
+#include <stdexcept>
 
 namespace {
-// is only used internally in this class but is less than nice 
-// to keep here. 
-// Kjell's suggestion: 
-// I would keep it here and move it later when/if the validation
-// is extracted to its own class,. which should? be done when 
-// the other conf are being validated
+   // is only used internally in this class but is less than nice 
+   // to keep here. 
+   // Kjell's suggestion: 
+   // I would keep it here and move it later when/if the validation
+   // is extracted to its own class,. which should? be done when 
+   // the other conf are being validated
 
-class ConfValidationException : public std::runtime_error {
-public:
+   class ConfValidationException : public std::runtime_error {
+   public:
 
-    /// constructs an internal exception
+      /// constructs an internal exception
 
-    explicit ConfValidationException(const std::string& msg)
-    : std::runtime_error(msg) {
-    }
-};
+      explicit ConfValidationException(const std::string& msg)
+      : std::runtime_error(msg) {
+      }
+   };
 }
 
 ValidateChecks::ValidateChecks() {
@@ -36,15 +38,15 @@ ValidateChecks::~ValidateChecks() {
  * @throws on illegal values
  */
 bool ValidateChecks::CheckNumber(const std::string& number, const Range& range) {
-    try {
-        CheckNumberForNegative(number);
-        CheckNumberForSize(number, range);
-    } catch (std::exception e) {
-        LOG(WARNING) << "Number not accepted: " << e.what();
-        return false;
-    }
+   try {
+      CheckNumberForNegative(number);
+      CheckNumberForSize(number, range);
+   } catch (std::exception e) {
+      LOG(WARNING) << "Number not accepted: " << e.what();
+      return false;
+   }
 
-    return true;
+   return true;
 }
 
 /**
@@ -53,12 +55,12 @@ bool ValidateChecks::CheckNumber(const std::string& number, const Range& range) 
  * @throws on illegal values
  */
 void ValidateChecks::CheckNumberForNegative(const std::string& number) {
-    int32_t value = std::stoi(number);
-    if (value < 0) {
-        std::string error = {"Negative values not accepted in BaseConf : "};
-        error.append(std::to_string(value));
-        throw ConfValidationException(error);
-    }
+   int32_t value = std::stoi(number);
+   if (value < 0) {
+      std::string error = {"Negative values not accepted in BaseConf : "};
+      error.append(std::to_string(value));
+      throw ConfValidationException(error);
+   }
 }
 
 /**
@@ -68,16 +70,16 @@ void ValidateChecks::CheckNumberForNegative(const std::string& number) {
  * @throws on illegal values
  */
 void ValidateChecks::CheckNumberForSize(const std::string& number, const Range& range) {
-    long long value = std::stoll(number);
+   long long value = std::stoll(number);
 
-    if (!range.WithinRange(value)) {
-        std::string error = {"Not allowed value. Must be within range of: "};
-        error.append(std::to_string(range.lower));
-        error.append("  - ").append(std::to_string(range.higher));
-        error.append(". This value was: ").append(std::to_string(value));
-        error.append(std::to_string(value));
-        throw ConfValidationException(error);
-    }
+   if (!range.WithinRange(value)) {
+      std::string error = {"Not allowed value. Must be within range of: "};
+      error.append(std::to_string(range.lower));
+      error.append("  - ").append(std::to_string(range.higher));
+      error.append(". This value was: ").append(std::to_string(value));
+      error.append(std::to_string(value));
+      throw ConfValidationException(error);
+   }
 }
 
 /**
@@ -86,14 +88,14 @@ void ValidateChecks::CheckNumberForSize(const std::string& number, const Range& 
  * @return true if legal to use
  */
 bool ValidateChecks::CheckString(const std::string& text) {
-    try {
-        CheckStringForSize(text);
-    } catch (std::exception e) {
-        LOG(WARNING) << "Text/String value not accepted: " << e.what();
-        return false;
-    }
+   try {
+      CheckStringForSize(text);
+   } catch (std::exception e) {
+      LOG(WARNING) << "Text/String value not accepted: " << e.what();
+      return false;
+   }
 
-    return true;
+   return true;
 }
 
 /**
@@ -102,13 +104,13 @@ bool ValidateChecks::CheckString(const std::string& text) {
  * @throws on illegal size
  */
 void ValidateChecks::CheckStringForSize(const std::string& text) {
-    static const size_t maxSize = 1000; // too big on purpose, trimming it down later?
-    const size_t size = text.size();
-    if (size > maxSize) {
-        std::string error = {"Too big string, max is 1000, this was: "};
-        error.append(std::to_string(size));
-        throw ConfValidationException(error);
-    }
+   static const size_t maxSize = 1000; // too big on purpose, trimming it down later?
+   const size_t size = text.size();
+   if (size > maxSize) {
+      std::string error = {"Too big string, max is 1000, this was: "};
+      error.append(std::to_string(size));
+      throw ConfValidationException(error);
+   }
 }
 
 /**
@@ -117,11 +119,11 @@ void ValidateChecks::CheckStringForSize(const std::string& text) {
  * @return true if bool can be extracted from string
  */
 bool ValidateChecks::CheckBool(const std::string& text) {
-    static const std::string True = {"true"};
-    static const std::string False = {"false"};
-    if (text == True || text == False) {
-        return true;
-    }
-    LOG(WARNING) << "Expected bool, received value of size: " << text.size();
-    return false;
+   static const std::string True = {"true"};
+   static const std::string False = {"false"};
+   if (text == True || text == False) {
+      return true;
+   }
+   LOG(WARNING) << "Expected bool, received value of size: " << text.size();
+   return false;
 }
--- ValidateChecks.h
+++ ValidateChecks.h
@@ -6,26 +6,20 @@
  */
 #pragma once
 
-#include <map>
-#include <string>
-#include <sstream>
-#include <stdexcept>
-#include "BaseConfMsg.pb.h"
-#include "SyslogConfMsg.pb.h"
-#include "QosmosConf.h"
 #include "include/global.h"
+#include <string>
 #include "Range.h"
 
 struct ValidateChecks {
-    ValidateChecks();
-    virtual ~ValidateChecks();
-    LR_VIRTUAL bool CheckNumber(const std::string& number, const Range& range);
-    LR_VIRTUAL void CheckNumberForNegative(const std::string& number); // throw
-    LR_VIRTUAL void CheckNumberForSize(const std::string& number, const Range& range); // throw
+   ValidateChecks();
+   virtual ~ValidateChecks();
+   LR_VIRTUAL bool CheckNumber(const std::string& number, const Range& range);
+   LR_VIRTUAL void CheckNumberForNegative(const std::string& number); // throw
+   LR_VIRTUAL void CheckNumberForSize(const std::string& number, const Range& range); // throw
 
-    LR_VIRTUAL bool CheckString(const std::string& text);
-    LR_VIRTUAL void CheckStringForSize(const std::string& text); // throw
+   LR_VIRTUAL bool CheckString(const std::string& text);
+   LR_VIRTUAL void CheckStringForSize(const std::string& text); // throw
 
-    LR_VIRTUAL bool CheckBool(const std::string& text);
+   LR_VIRTUAL bool CheckBool(const std::string& text);
 };
 
