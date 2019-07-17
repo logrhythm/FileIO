02c8bfff3de6a14705e7b0264c92c0d37d81df3e
Elizabeth Jarrett <m.elizabeth.jarrett@gmail.com>
2019-07-17 11:31:04 -0600

    Clean out rpmbuild dir before building

--- scripts/buildRpm.sh
+++ scripts/buildRpm.sh
@@ -32,6 +32,7 @@ CWD=$PWD/$PACKAGE
 DISTDIR=$CWD/dist/$PACKAGE
 PATH=$PATH:/usr/local/probe/bin:$PATH
 
+sudo rm -rf ~/rpmbuild
 rpmdev-setuptree
 cp packaging/$PACKAGE.spec ~/rpmbuild/SPECS
 rm -f $PACKAGE-$VERSION.tar.gz
